708a243d9be79c4abbb52e28b9dbbb865c733814
Yiping Chen <yping.chen@mail.utoronto.ca>
2024-10-28 16:38:53 -0400

    task 2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Returns the user with the given username.
+     * @param name the username to set
+     */
+    void setCurrentUser(String name);
 }
